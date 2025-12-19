0f9677ba7dbe643a6d02f3cf31dbe11f6d0f78b7
Fxhril <mfahril779@gmail.com>
2025-12-19 20:10:32 +0700

    Menghapus "set experiment mlflow" agar tidak bertabrakan dengan github actions

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -10,9 +10,6 @@ X = df.drop('target', axis=1)
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 2. Setup MLflow (LOKAL)
-mlflow.set_experiment("Eksperimen_Basic_Lokal")
-
 with mlflow.start_run():
     mlflow.autolog()
 
